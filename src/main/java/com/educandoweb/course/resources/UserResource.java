46298fc9e5e817f9cdf8da018c2b2da4c3d3c848
Vinicius <viniciusmendes2802@gmail.com>
2024-05-02 20:03:01 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,5 +1,8 @@
 package com.educandoweb.course.resources;
 
+
+
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
